f0d3398a1ea4b49543e3f99209be0b24d27bcde3
TechNikFuture <108083158+TechNikFuture@users.noreply.github.com>
2023-07-29 23:14:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-Fach_1_Rechte = "01, "         // Auf Fach 1 haben folgende Zahlen Zugriff
+Fach_1_Rechte = "02, "         // Auf Fach 1 haben folgende Zahlen Zugriff
 Fach_1_Arbeit_oder_Test = "Arbeit"
 Fach_1_Seitenanzahl = 1
 
-Guthaben_1 = "1,5"
+Guthaben_1 = "2,5"
 
 
 
