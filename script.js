f8c12e15a28bbe7ed8d1de72cd95e5c4c3c03c29
Tech Nik <tech.nik.effects@gmail.com>
2023-07-30 19:23:57 +0200

    f

--- script.js
+++ script.js
@@ -32,18 +32,24 @@ Guthaben_1 = "1,5"
 
 
 
-function loadScript( url, callback ) {
-    var script = document.createElement( "script" )
-          script.type = "text/javascript";
-        script.src = url;
-         script.onload = function() {
-           callback()
-        };
-       document.head.appendChild(script); 
+function loadJS(FILE_URL, async = true) {
+    let scriptEle = document.createElement("script");
+  
+    scriptEle.setAttribute("src", FILE_URL);
+    scriptEle.setAttribute("type", "text/javascript");
+    scriptEle.setAttribute("async", async);
+  
+    document.body.appendChild(scriptEle);
+  
+    // success event 
+    scriptEle.addEventListener("load", () => {
+      console.log("File loaded")
+    });
+     // error event
+    scriptEle.addEventListener("error", (ev) => {
+      console.log("Error on loading file", ev);
+    });
   }
-  // call the function...
-  loadScript("/Fach_1.js", function() {
-    alert('script ready!'); 
-  });
-  
-  
+  loadJS("/Fach_1.js", true);
+
+
