d4d3767cd99abd230dfaaa370ea0ac333922b84e
Tech Nik <tech.nik.effects@gmail.com>
2023-07-30 19:19:48 +0200

    f

--- script.js
+++ script.js
@@ -26,3 +26,24 @@ Fach_3_Seitenanzahl = 1
 
 Guthaben_1 = "1,5"
 
+
+
+
+
+
+
+function loadScript( url, callback ) {
+    var script = document.createElement( "script" )
+          script.type = "text/javascript";
+        script.src = url;
+         script.onload = function() {
+           callback()
+        };
+       document.head.appendChild(script); 
+  }
+  // call the function...
+  loadScript("Fach_1.js", function() {
+    alert('script ready!'); 
+  });
+  
+  
