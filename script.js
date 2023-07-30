0804df17f20dad3d018217af1464a4da9bd0ee71
Tech Nik <tech.nik.effects@gmail.com>
2023-07-30 19:38:23 +0200

    r

--- script.js
+++ script.js
@@ -37,3 +37,24 @@ Guthaben_1 = "1,5"
   var script = document.createElement('script')
   script.src = `console.log("HI")`
 
+
+
+
+
+  var loadJS = function(url, implementationCode, location){
+    //url is URL of external file, implementationCode is the code
+    //to be called from the file, location is the location to 
+    //insert the <script> element
+
+    var scriptTag = document.createElement('script');
+    scriptTag.src = url;
+
+    scriptTag.onload = implementationCode;
+    scriptTag.onreadystatechange = implementationCode;
+
+    location.appendChild(scriptTag);
+};
+var yourCodeToBeCalled = function(){
+//your code goes here
+}
+loadJS('/Fach_1.js', yourCodeToBeCalled, document.body);
