d09f61f4dfc8ce24477f92497e672de15253ffd7
Tech Nik <tech.nik.effects@gmail.com>
2023-07-31 02:26:40 +0200

    e

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ Fach_1_Seitenanzahl = 2
 ///////////////////
 ///////////////////
 
-Fach_2_Fach = "Mathe Arbeit"
-Fach_2_Rechte = ""
-Fach_2_Seitenanzahl = 1
+Fach_2_Fach = "Musik Arbeit"
+Fach_2_Rechte = "01"
+Fach_2_Seitenanzahl = 4
 
 
 ///////////////////
 ///////////////////
 
-Fach_3_Fach = "Bio Test"
+Fach_3_Fach = "Bio Arbeit"
 Fach_3_Rechte = "01, "
-Fach_3_Seitenanzahl = 1
+Fach_3_Seitenanzahl = 7
 
 
 ///////////////////
