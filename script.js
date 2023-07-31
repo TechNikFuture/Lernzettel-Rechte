08db3e258d5d30fc0c37c56ea0cac74f08af5b9b
Tech Nik <tech.nik.effects@gmail.com>
2023-08-01 00:50:19 +0200

    d

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 Fach_1_Fach = "Kunst Arbeit"
-Fach_1_Rechte = ""
+Fach_1_Rechte = "01, "
 Fach_1_Seitenanzahl = 2
 
 
@@ -7,7 +7,7 @@ Fach_1_Seitenanzahl = 2
 ///////////////////
 
 Fach_2_Fach = "Musik Arbeit"
-Fach_2_Rechte = "02"
+Fach_2_Rechte = "01"
 Fach_2_Seitenanzahl = 4
 
 
@@ -32,32 +32,3 @@ Fach_2_URL = `<script src="https://lernzettel-rechte.netlify.app/Fach_2.js"></sc
 Fach_3_URL = `<script src="https://lernzettel-rechte.netlify.app/Fach_3.js"></script>`
 
 
-
-
-
-breite_1_1 = 130
-höhe_1_1 = 20.5
-
-breite_1_2 = 130
-höhe_1_2 = 20.5
-
-breite_2_1 = 130
-höhe_2_1 = 20.5
-
-breite_2_2 = 130
-höhe_2_2 = 20.5
-
-breite_3_1 = 130
-höhe_3_1 = 20.5
-
-breite_3_2 = 130
-höhe_3_2 = 20.5
-
-breite_4 = 130
-höhe_4 = 20.5
-
-breite_6 = 130
-höhe_6 = 20.5
-
-breite_7 = 130
-höhe_7 = 20.5
