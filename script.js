eadd80aec13f9a98398c86b025bef187365335ce
Tech Nik <tech.nik.effects@gmail.com>
2023-08-01 02:25:02 +0200

    e

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 Fach_1_Fach = "Kunst Arbeit"
-Fach_1_Rechte = "01, "
+Fach_1_Rechte = "01, 02, 03, "
 Fach_1_Seitenanzahl = 2
 
 
@@ -7,7 +7,7 @@ Fach_1_Seitenanzahl = 2
 ///////////////////
 
 Fach_2_Fach = "Musik Arbeit"
-Fach_2_Rechte = "01"
+Fach_2_Rechte = "01, 02, 03, "
 Fach_2_Seitenanzahl = 4
 
 
@@ -15,7 +15,7 @@ Fach_2_Seitenanzahl = 4
 ///////////////////
 
 Fach_3_Fach = "Bio Arbeit"
-Fach_3_Rechte = "01, "
+Fach_3_Rechte = "01, 02, 03, "
 Fach_3_Seitenanzahl = 7
 
 
