00259014b290b547d490a057d2da7ec400ec7306
Tech Nik <tech.nik.effects@gmail.com>
2023-08-01 22:28:19 +0200

    d

--- script.js
+++ script.js
@@ -23,6 +23,531 @@ Guthaben_20 /**/ = "0 €"
 
 
 
+Lz_50_Fach = ""
+Lz_50_Jahrgang = 10
+Lz_50_Rechte = ""
+Lz_50_Seitenanzahl = 0
+Lz_50_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_49_Fach = ""
+Lz_49_Jahrgang = 10
+Lz_49_Rechte = ""
+Lz_49_Seitenanzahl = 0
+Lz_49_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_48_Fach = ""
+Lz_48_Jahrgang = 10
+Lz_48_Rechte = ""
+Lz_48_Seitenanzahl = 0
+Lz_48_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_47_Fach = ""
+Lz_47_Jahrgang = 10
+Lz_47_Rechte = ""
+Lz_47_Seitenanzahl = 0
+Lz_47_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_46_Fach = ""
+Lz_46_Jahrgang = 10
+Lz_46_Rechte = ""
+Lz_46_Seitenanzahl = 0
+Lz_46_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_45_Fach = ""
+Lz_45_Jahrgang = 10
+Lz_45_Rechte = ""
+Lz_45_Seitenanzahl = 0
+Lz_45_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_44_Fach = ""
+Lz_44_Jahrgang = 10
+Lz_44_Rechte = ""
+Lz_44_Seitenanzahl = 0
+Lz_44_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_43_Fach = ""
+Lz_43_Jahrgang = 10
+Lz_43_Rechte = ""
+Lz_43_Seitenanzahl = 0
+Lz_43_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_42_Fach = ""
+Lz_42_Jahrgang = 10
+Lz_42_Rechte = ""
+Lz_42_Seitenanzahl = 0
+Lz_42_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_41_Fach = ""
+Lz_41_Jahrgang = 10
+Lz_41_Rechte = ""
+Lz_41_Seitenanzahl = 0
+Lz_41_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_40_Fach = ""
+Lz_40_Jahrgang = 10
+Lz_40_Rechte = ""
+Lz_40_Seitenanzahl = 0
+Lz_40_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_39_Fach = ""
+Lz_39_Jahrgang = 10
+Lz_39_Rechte = ""
+Lz_39_Seitenanzahl = 0
+Lz_39_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_38_Fach = ""
+Lz_38_Jahrgang = 10
+Lz_38_Rechte = ""
+Lz_38_Seitenanzahl = 0
+Lz_38_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_37_Fach = ""
+Lz_37_Jahrgang = 10
+Lz_37_Rechte = ""
+Lz_37_Seitenanzahl = 0
+Lz_37_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_36_Fach = ""
+Lz_36_Jahrgang = 10
+Lz_36_Rechte = ""
+Lz_36_Seitenanzahl = 0
+Lz_36_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_35_Fach = ""
+Lz_35_Jahrgang = 10
+Lz_35_Rechte = ""
+Lz_35_Seitenanzahl = 0
+Lz_35_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_34_Fach = ""
+Lz_34_Jahrgang = 10
+Lz_34_Rechte = ""
+Lz_34_Seitenanzahl = 0
+Lz_34_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_33_Fach = ""
+Lz_33_Jahrgang = 10
+Lz_33_Rechte = ""
+Lz_33_Seitenanzahl = 0
+Lz_33_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_32_Fach = ""
+Lz_32_Jahrgang = 10
+Lz_32_Rechte = ""
+Lz_32_Seitenanzahl = 0
+Lz_32_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_31_Fach = ""
+Lz_31_Jahrgang = 10
+Lz_31_Rechte = ""
+Lz_31_Seitenanzahl = 0
+Lz_31_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_30_Fach = ""
+Lz_30_Jahrgang = 10
+Lz_30_Rechte = ""
+Lz_30_Seitenanzahl = 0
+Lz_30_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_29_Fach = ""
+Lz_29_Jahrgang = 10
+Lz_29_Rechte = ""
+Lz_29_Seitenanzahl = 0
+Lz_29_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_28_Fach = ""
+Lz_28_Jahrgang = 10
+Lz_28_Rechte = ""
+Lz_28_Seitenanzahl = 0
+Lz_28_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_27_Fach = ""
+Lz_27_Jahrgang = 10
+Lz_27_Rechte = ""
+Lz_27_Seitenanzahl = 0
+Lz_27_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_26_Fach = ""
+Lz_26_Jahrgang = 10
+Lz_26_Rechte = ""
+Lz_26_Seitenanzahl = 0
+Lz_26_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_25_Fach = ""
+Lz_25_Jahrgang = 10
+Lz_25_Rechte = ""
+Lz_25_Seitenanzahl = 0
+Lz_25_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_24_Fach = ""
+Lz_24_Jahrgang = 10
+Lz_24_Rechte = ""
+Lz_24_Seitenanzahl = 0
+Lz_24_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_23_Fach = ""
+Lz_23_Jahrgang = 10
+Lz_23_Rechte = ""
+Lz_23_Seitenanzahl = 0
+Lz_23_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_22_Fach = ""
+Lz_22_Jahrgang = 10
+Lz_22_Rechte = ""
+Lz_22_Seitenanzahl = 0
+Lz_22_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_21_Fach = ""
+Lz_21_Jahrgang = 10
+Lz_21_Rechte = ""
+Lz_21_Seitenanzahl = 0
+Lz_21_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_20_Fach = ""
+Lz_20_Jahrgang = 10
+Lz_20_Rechte = ""
+Lz_20_Seitenanzahl = 0
+Lz_20_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_19_Fach = ""
+Lz_19_Jahrgang = 10
+Lz_19_Rechte = ""
+Lz_19_Seitenanzahl = 0
+Lz_19_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_18_Fach = ""
+Lz_18_Jahrgang = 10
+Lz_18_Rechte = ""
+Lz_18_Seitenanzahl = 0
+Lz_18_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_17_Fach = ""
+Lz_17_Jahrgang = 10
+Lz_17_Rechte = ""
+Lz_17_Seitenanzahl = 0
+Lz_17_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_16_Fach = ""
+Lz_16_Jahrgang = 10
+Lz_16_Rechte = ""
+Lz_16_Seitenanzahl = 0
+Lz_16_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_15_Fach = ""
+Lz_15_Jahrgang = 10
+Lz_15_Rechte = ""
+Lz_15_Seitenanzahl = 0
+Lz_15_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_14_Fach = ""
+Lz_14_Jahrgang = 10
+Lz_14_Rechte = ""
+Lz_14_Seitenanzahl = 0
+Lz_14_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_13_Fach = ""
+Lz_13_Jahrgang = 10
+Lz_13_Rechte = ""
+Lz_13_Seitenanzahl = 0
+Lz_13_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_12_Fach = ""
+Lz_12_Jahrgang = 10
+Lz_12_Rechte = ""
+Lz_12_Seitenanzahl = 0
+Lz_12_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_11_Fach = ""
+Lz_11_Jahrgang = 10
+Lz_11_Rechte = ""
+Lz_11_Seitenanzahl = 0
+Lz_11_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_10_Fach = ""
+Lz_10_Jahrgang = 10
+Lz_10_Rechte = ""
+Lz_10_Seitenanzahl = 0
+Lz_10_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_9_Fach = ""
+Lz_9_Jahrgang = 10
+Lz_9_Rechte = ""
+Lz_9_Seitenanzahl = 0
+Lz_9_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_8_Fach = ""
+Lz_8_Jahrgang = 10
+Lz_8_Rechte = ""
+Lz_8_Seitenanzahl = 0
+Lz_8_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_7_Fach = ""
+Lz_7_Jahrgang = 10
+Lz_7_Rechte = ""
+Lz_7_Seitenanzahl = 0
+Lz_7_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_6_Fach = ""
+Lz_6_Jahrgang = 10
+Lz_6_Rechte = ""
+Lz_6_Seitenanzahl = 0
+Lz_6_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_5_Fach = ""
+Lz_5_Jahrgang = 10
+Lz_5_Rechte = ""
+Lz_5_Seitenanzahl = 0
+Lz_5_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+Lz_4_Fach = ""
+Lz_4_Jahrgang = 10
+Lz_4_Rechte = ""
+Lz_4_Seitenanzahl = 0
+Lz_4_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/"></script>`
+
+
+///////////////////
+///////////////////
+
+
+
+
+
+
+
+
+
+
 
 
 Lz_3_Fach = "Bio Arbeit"
@@ -54,4 +579,5 @@ Lz_1_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/04 - Kuns
 
 ///////////////////
 ///////////////////
+
 
