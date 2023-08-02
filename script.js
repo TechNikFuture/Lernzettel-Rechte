5f147180d783e8c3296740ddc616034214afa6ce
Tech Nik <tech.nik.effects@gmail.com>
2023-08-03 00:14:40 +0200

    d

--- script.js
+++ script.js
@@ -42,7 +42,6 @@ Guthaben_35 = "0 €" /**/
 
 
 Lz_50_Fach = ""
-Lz_50_Jahrgang = 9
 Lz_50_Rechte = "root, "
 Lz_50_Seitenanzahl = 0
 Lz_50_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -53,7 +52,6 @@ Lz_50_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_49_Fach = ""
-Lz_49_Jahrgang = 9
 Lz_49_Rechte = "root, "
 Lz_49_Seitenanzahl = 0
 Lz_49_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -64,7 +62,6 @@ Lz_49_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_48_Fach = ""
-Lz_48_Jahrgang = 9
 Lz_48_Rechte = "root, "
 Lz_48_Seitenanzahl = 0
 Lz_48_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -75,7 +72,6 @@ Lz_48_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_47_Fach = ""
-Lz_47_Jahrgang = 9
 Lz_47_Rechte = "root, "
 Lz_47_Seitenanzahl = 0
 Lz_47_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -86,7 +82,6 @@ Lz_47_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_46_Fach = ""
-Lz_46_Jahrgang = 9
 Lz_46_Rechte = "root, "
 Lz_46_Seitenanzahl = 0
 Lz_46_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -97,7 +92,6 @@ Lz_46_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_45_Fach = ""
-Lz_45_Jahrgang = 9
 Lz_45_Rechte = "root, "
 Lz_45_Seitenanzahl = 0
 Lz_45_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -108,7 +102,6 @@ Lz_45_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_44_Fach = ""
-Lz_44_Jahrgang = 9
 Lz_44_Rechte = "root, "
 Lz_44_Seitenanzahl = 0
 Lz_44_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -119,7 +112,6 @@ Lz_44_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_43_Fach = ""
-Lz_43_Jahrgang = 9
 Lz_43_Rechte = "root, "
 Lz_43_Seitenanzahl = 0
 Lz_43_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -130,7 +122,6 @@ Lz_43_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_42_Fach = ""
-Lz_42_Jahrgang = 9
 Lz_42_Rechte = "root, "
 Lz_42_Seitenanzahl = 0
 Lz_42_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -141,7 +132,6 @@ Lz_42_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_41_Fach = ""
-Lz_41_Jahrgang = 9
 Lz_41_Rechte = "root, "
 Lz_41_Seitenanzahl = 0
 Lz_41_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -152,7 +142,6 @@ Lz_41_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_40_Fach = ""
-Lz_40_Jahrgang = 9
 Lz_40_Rechte = "root, "
 Lz_40_Seitenanzahl = 0
 Lz_40_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -163,7 +152,6 @@ Lz_40_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_39_Fach = ""
-Lz_39_Jahrgang = 9
 Lz_39_Rechte = "root, "
 Lz_39_Seitenanzahl = 0
 Lz_39_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -174,7 +162,6 @@ Lz_39_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_38_Fach = ""
-Lz_38_Jahrgang = 9
 Lz_38_Rechte = "root, "
 Lz_38_Seitenanzahl = 0
 Lz_38_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -185,7 +172,6 @@ Lz_38_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_37_Fach = ""
-Lz_37_Jahrgang = 9
 Lz_37_Rechte = "root, "
 Lz_37_Seitenanzahl = 0
 Lz_37_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -196,7 +182,6 @@ Lz_37_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_36_Fach = ""
-Lz_36_Jahrgang = 9
 Lz_36_Rechte = "root, "
 Lz_36_Seitenanzahl = 0
 Lz_36_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -207,7 +192,6 @@ Lz_36_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_35_Fach = ""
-Lz_35_Jahrgang = 9
 Lz_35_Rechte = "root, "
 Lz_35_Seitenanzahl = 0
 Lz_35_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -218,7 +202,6 @@ Lz_35_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_34_Fach = ""
-Lz_34_Jahrgang = 9
 Lz_34_Rechte = "root, "
 Lz_34_Seitenanzahl = 0
 Lz_34_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -229,7 +212,6 @@ Lz_34_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_33_Fach = ""
-Lz_33_Jahrgang = 9
 Lz_33_Rechte = "root, "
 Lz_33_Seitenanzahl = 0
 Lz_33_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -240,7 +222,6 @@ Lz_33_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_32_Fach = ""
-Lz_32_Jahrgang = 9
 Lz_32_Rechte = "root, "
 Lz_32_Seitenanzahl = 0
 Lz_32_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -251,7 +232,6 @@ Lz_32_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_31_Fach = ""
-Lz_31_Jahrgang = 9
 Lz_31_Rechte = "root, "
 Lz_31_Seitenanzahl = 0
 Lz_31_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -262,7 +242,6 @@ Lz_31_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_30_Fach = ""
-Lz_30_Jahrgang = 9
 Lz_30_Rechte = "root, "
 Lz_30_Seitenanzahl = 0
 Lz_30_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -273,7 +252,6 @@ Lz_30_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_29_Fach = ""
-Lz_29_Jahrgang = 9
 Lz_29_Rechte = "root, "
 Lz_29_Seitenanzahl = 0
 Lz_29_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -284,7 +262,6 @@ Lz_29_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_28_Fach = ""
-Lz_28_Jahrgang = 9
 Lz_28_Rechte = "root, "
 Lz_28_Seitenanzahl = 0
 Lz_28_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -295,7 +272,6 @@ Lz_28_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_27_Fach = ""
-Lz_27_Jahrgang = 9
 Lz_27_Rechte = "root, "
 Lz_27_Seitenanzahl = 0
 Lz_27_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -306,7 +282,6 @@ Lz_27_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_26_Fach = ""
-Lz_26_Jahrgang = 9
 Lz_26_Rechte = "root, "
 Lz_26_Seitenanzahl = 0
 Lz_26_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -317,7 +292,6 @@ Lz_26_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_25_Fach = ""
-Lz_25_Jahrgang = 9
 Lz_25_Rechte = "root, "
 Lz_25_Seitenanzahl = 0
 Lz_25_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -328,7 +302,6 @@ Lz_25_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_24_Fach = ""
-Lz_24_Jahrgang = 9
 Lz_24_Rechte = "root, "
 Lz_24_Seitenanzahl = 0
 Lz_24_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -339,7 +312,6 @@ Lz_24_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_23_Fach = ""
-Lz_23_Jahrgang = 9
 Lz_23_Rechte = "root, "
 Lz_23_Seitenanzahl = 0
 Lz_23_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -350,7 +322,6 @@ Lz_23_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_22_Fach = ""
-Lz_22_Jahrgang = 9
 Lz_22_Rechte = "root, "
 Lz_22_Seitenanzahl = 0
 Lz_22_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -361,7 +332,6 @@ Lz_22_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_21_Fach = ""
-Lz_21_Jahrgang = 9
 Lz_21_Rechte = "root, "
 Lz_21_Seitenanzahl = 0
 Lz_21_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -372,7 +342,6 @@ Lz_21_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_20_Fach = ""
-Lz_20_Jahrgang = 9
 Lz_20_Rechte = "root, "
 Lz_20_Seitenanzahl = 0
 Lz_20_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -383,7 +352,6 @@ Lz_20_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_19_Fach = ""
-Lz_19_Jahrgang = 9
 Lz_19_Rechte = "root, "
 Lz_19_Seitenanzahl = 0
 Lz_19_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -394,7 +362,6 @@ Lz_19_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_18_Fach = ""
-Lz_18_Jahrgang = 9
 Lz_18_Rechte = "root, "
 Lz_18_Seitenanzahl = 0
 Lz_18_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -405,7 +372,6 @@ Lz_18_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_17_Fach = ""
-Lz_17_Jahrgang = 9
 Lz_17_Rechte = "root, "
 Lz_17_Seitenanzahl = 0
 Lz_17_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -416,7 +382,6 @@ Lz_17_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_16_Fach = ""
-Lz_16_Jahrgang = 9
 Lz_16_Rechte = "root, "
 Lz_16_Seitenanzahl = 0
 Lz_16_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
@@ -428,7 +393,6 @@ Lz_16_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
 
 Lz_14_Fach = "Kunst Arbeit: Porträtanalyse"
-Lz_14_Jahrgang = 9
 Lz_14_Rechte = "root, 01, 15, 02, 13, 29, 22, 03, 30, 16, 17, 28"
 Lz_14_Seitenanzahl = 2
 Lz_14_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/04 - Kunst/09 2. Hj. - Porträtanalyse.js"></script>`
@@ -439,7 +403,6 @@ Lz_14_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/04 - Kun
 
 
 Lz_13_Fach = "Politik Arbeit: Amazon + Startups"
-Lz_13_Jahrgang = 9
 Lz_13_Rechte = "root, 04, 27, 16, 02, 12, 03, 10, 07, 28, 14, 15"
 Lz_13_Seitenanzahl = 3
 Lz_13_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/03 - Politik/09 2. Hj. - Arbeit - Amazon + Startups.js"></script>`
@@ -450,7 +413,6 @@ Lz_13_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/03 - Pol
 
 
 Lz_12_Fach = "Chemie Arbeit: Atom + Elektronenkonfig. + Gleichungen"
-Lz_12_Jahrgang = 9
 Lz_12_Rechte = "root, 27, 02, 12, 07, 04, 14"
 Lz_12_Seitenanzahl = 5
 Lz_12_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/06 - Chemie/09 2. Hj. - Arbeit - Atom + Elektronenkonfig. + Gleichungen.js"></script>`
@@ -461,7 +423,6 @@ Lz_12_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/06 - Che
 
 
 Lz_11_Fach = "Bio Arbeit: Auge + Ohr + Bakterien und Viren"
-Lz_11_Jahrgang = 9
 Lz_11_Rechte = "root, 12, 07, 08, 27, 14, 15, 02, 04"
 Lz_11_Seitenanzahl = 7
 Lz_11_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/07 - Bio/09 2. Hj. - Arbeit - Auge Komplett + Ohr + Bakterien und Viren.js"></script>`
@@ -472,7 +433,6 @@ Lz_11_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/07 - Bio
 
 
 Lz_10_Fach = "Reli Arbeit: Buddhismus"
-Lz_10_Jahrgang = 9
 Lz_10_Rechte = "root, 02, 07, 01, 16"
 Lz_10_Seitenanzahl = 3
 Lz_10_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/09 - Reli/09 2. Hj. - Arbeit - Buddhismus.js"></script>`
@@ -483,7 +443,6 @@ Lz_10_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/09 - Rel
 
 
 Lz_9_Fach = "Musik Arbeit: Epochen + Barock"
-Lz_9_Jahrgang = 9
 Lz_9_Rechte = "root, 26, 03, 02, 07"
 Lz_9_Seitenanzahl = 4
 Lz_9_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/08 - Musik/09 2. Hj. - Arbeit - Epochen + Barock.js"></script>`
@@ -494,7 +453,6 @@ Lz_9_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/08 - Musi
 
 
 Lz_8_Fach = "Erdkunde Arbeit: Botswana"
-Lz_8_Jahrgang = 9
 Lz_8_Rechte = "root, 01, 02, 03, 16, 12, 03, 10, 07, 20, 21, 06, 04, 15, 14, 22, 23, 24, 25"
 Lz_8_Seitenanzahl = 8
 Lz_8_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/11 - Erdkunde/09 2. Hj. - Arbeit - Botswana.js"></script>`
@@ -505,7 +463,6 @@ Lz_8_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/11 - Erdk
 
 
 Lz_15_Fach = "Mathe Arbeit: Parabeln + p, q - Formel + Gleichungssysteme"
-Lz_15_Jahrgang = 9
 Lz_15_Rechte = "root, 02, 03, 13, 04, 19, 12"
 Lz_15_Seitenanzahl = 9
 Lz_15_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/02 - Mathe/09 2. Hj. - Arbeit - Parabeln + p, q - Formel + Gleichungssysteme.js"></script>`
@@ -516,7 +473,6 @@ Lz_15_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/02 - Mat
 
 
 Lz_7_Fach = "Mathe Arbeit: Parabeln"
-Lz_7_Jahrgang = 9
 Lz_7_Rechte = "root, 01, 13, 14, 02, 09, 12, 04"
 Lz_7_Seitenanzahl = 4
 Lz_7_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/02 - Mathe/09 1. Hj. - Arbeit - Parabeln.js"></script>`
@@ -527,7 +483,6 @@ Lz_7_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/02 - Math
 
 
 Lz_6_Fach = "Kunst Arbeit: Design Analyse"
-Lz_6_Jahrgang = 9
 Lz_6_Rechte = "root, 01, 02, 03, 10, 18"
 Lz_6_Seitenanzahl = 3
 Lz_6_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/04 - Kunst/09 1. Hj. - Arbeit - Design Analyse.js"></script>`
@@ -538,7 +493,6 @@ Lz_6_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/04 - Kuns
 
 
 Lz_5_Fach = "Deutsch Arbeit: Generell"
-Lz_5_Jahrgang = 9
 Lz_5_Rechte = "root, 01, 03, 17, 02, 18, 04"
 Lz_5_Seitenanzahl = 3
 Lz_5_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/01 - Deutsch/09 1. Hj. - Arbeit - Generell.js"></script>`
@@ -549,7 +503,6 @@ Lz_5_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/01 - Deut
 
 
 Lz_4_Fach = "Politik Arbeit: Bundesregierung"
-Lz_4_Jahrgang = 9
 Lz_4_Rechte = "root, 03, 01, 02, 16, 15, 09"
 Lz_4_Seitenanzahl = 2
 Lz_4_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/03 - Politik/09 1. Hj. - Arbeit - Bundesregierung.js"></script>`
@@ -560,7 +513,6 @@ Lz_4_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/03 - Poli
 
 
 Lz_3_Fach = "Physik Arbeit: Kinematik + Radioaktivität"
-Lz_3_Jahrgang = 9
 Lz_3_Rechte = "root, 01, 02, 13, 14, 10, 11, 05, 15, 12, 07, 16, 03, 04, 09"
 Lz_3_Seitenanzahl = 7
 Lz_3_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/05 - Physik/09 2. Hj. - Arbeit - Kinematik + Radioaktivität.js"></script>`
@@ -571,7 +523,6 @@ Lz_3_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/05 - Phys
 
 
 Lz_2_Fach = "Bio Arbeit: Evolutionstheorie + Tarnen + Variabilität"
-Lz_2_Jahrgang = 9
 Lz_2_Rechte = "root, 03, 04, 02, 10, 11, 12, 09"
 Lz_2_Seitenanzahl = 3
 Lz_2_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/07 - Bio/09 1. Hj. - Arbeit - Evolutionstheorie + Tarnen + Variabilität.js"></script>`
@@ -582,7 +533,6 @@ Lz_2_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/07 - Bio/
 
 
 Lz_1_Fach = "Geschichte Arbeit: Novemberrevolution bis Inflation 1923"
-Lz_1_Jahrgang = 9
 Lz_1_Rechte = "root, 02, 03, 04, 05, 07, 08, 09"
 Lz_1_Seitenanzahl = 7
 Lz_1_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/10 - Geschichte/09 1. Hj. - Arbeit - Novemberrevolution bis Inflation 1923.js"></script>`
