fd952bcaf1935a66bd5a7669ae493eac2e7e827b
TechNikFuture <108083158+TechNikFuture@users.noreply.github.com>
2023-09-20 14:13:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ Guthaben_35 = "0 €" /**/
 
 
 Lz_19_Fach = "Englisch Sprechprüfung"
-Lz_19_Rechte = "root, 01, 31, 29, 22"
+Lz_19_Rechte = "root, 01, 31, 29, 22, 02, 07"
 Lz_19_Seitenanzahl = 8
 Lz_19_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/13 - Englisch/10 1. Hj. - Sprechprüfung.js"></script>`
 
@@ -50,7 +50,7 @@ Lz_19_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/13 - Eng
 
 
 Lz_18_Fach = "Mathe Arbeit"
-Lz_18_Rechte = "root, Binar, Rayyan, Lucas"
+Lz_18_Rechte = "root, Binar, Rayyan, Lucas, Felix, Marlene"
 Lz_18_Seitenanzahl = 0
 Lz_18_URL = `<script src="https://lernzettel-rechte.netlify.app/"></script>`
 
