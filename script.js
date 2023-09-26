7d18a60a521c6f7e4d7d9cdb5fad3c37295de71e
TechNikFuture <108083158+TechNikFuture@users.noreply.github.com>
2023-09-26 19:26:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -70,7 +70,7 @@ Lz_17_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/01 - Deu
 
 
 Lz_16_Fach = "Chemie Test: Bindungen"
-Lz_16_Rechte = "root, 02, 07, 15"
+Lz_16_Rechte = "root, 02, 07, 15, 28"
 Lz_16_Seitenanzahl = 5
 Lz_16_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/06 - Chemie/10 1. Hj. - Test - Bindungen.js"></script>`
 
