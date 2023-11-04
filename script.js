99a7c58a173f5672b829fb1029af6388068026b8
Tech Nik <tech.nik.effects@gmail.com>
2023-11-04 20:07:45 +0100

    3

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ Guthaben_35 = "0 €" /**/
 
 
 Lz_21_Fach = "Informatik Arbeit"
-Lz_21_Rechte = "root, "
+Lz_21_Rechte = "root, 02, 07, "
 Lz_21_Seitenanzahl = 2
 Lz_21_URL = `<script src="https://lernzettel-rechte.netlify.app/Fächer/14 - Informatik/10 1. Hj.  - Scratch + Calliope.js"></script>`
 
